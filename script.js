dfdc6febc3c119d73cf12b6d150c70a9b7560262
JackBrasesco <none>
2016-09-21 08:38:45 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -8,3 +8,14 @@ setInterval(function() {
     backgroundIsBlue = true;
   }	
 }, 500)
+
+var borderIsBlue = false
+setInterval(function() {
+  if (borderIsBlue) {
+    $("#blue-blinker").css({background: "white"});
+    backgroundIsBlue = false;
+  } else {		
+    $("#blue-blinker").css({background: "blue"});	
+    backgroundIsBlue = true;
+  }	
+}, 100)
